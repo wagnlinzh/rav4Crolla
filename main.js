ff421e332fb02548a677093e093c5e0eb95163ca
wanglinzhizhi <wanglinzhizhi@hotmail.com>
2016-07-13 19:02:47 +0800

    readme upadte 关于模板

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 console.log("test2 online changes :)");
 require('./css/style.css');
 require('./css/main.css');
-var $=require('jquery');
+import $ from 'jquery';
+
+// var $ = require('jquery');
 
 
 // require('./index.html');
