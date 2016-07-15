3063c5eeb224b6aff7502db443d11a6ee468da2d
wanglinzhizhi <wanglinzhizhi@hotmail.com>
2016-07-15 14:56:35 +0800

    update readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var webpack=require('webpack');
+var webpack = require('webpack');
 
 var ROOT_PATH = path.resolve(__dirname);
 var DIST_PATH = path.resolve(ROOT_PATH, 'dist');
@@ -9,13 +9,13 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   //入口
   entry:
-  {    
-    bundle:'./main.js', //这个里面主要放的是各种css 等样式
+  {
+    bundle: './main.js', //这个里面主要放的是各种css 等样式
     // jquery:'./js/jquery1.8.3.min.js',
     // supperSlide:'./js/jquery.SuperSlide.js',
     // mScrollbar:'./js/jquery.mCustomScrollbar.min.js',
-    regCom:'./js/reg_com.js',
-    common:'./js/common.js',
+    regCom: './js/reg_com.js',
+    common: './js/common.js',
 
     //  wl:'./js/wl.js', 
     //  has:'./hah.js',
@@ -27,7 +27,7 @@ module.exports = {
   output: {
     path: DIST_PATH, // 输出文件的保存路径
     // filename: 'bundle.js', // 输出文件的名称
-    filename:'[name].[hash].js',
+    filename: '[name].[hash].js',
     // publicPath: "./dist/"
   },
 
@@ -43,14 +43,14 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style!css',
-        exclude:'node_modules'
+        exclude: 'node_modules'
       },
 
       {
         test: /\.(png|jpg|gif|jpeg)$/,
         loader: 'url-loader?limit=81920&name=./images/[hash].[ext]',
-        exclude:'node_modules'
-        
+        exclude: 'node_modules'
+
       },
 
       // {
@@ -63,9 +63,9 @@ module.exports = {
       // },
 
       {
-        test:/\.html$/,
-        loader:'html-loader',
-        exclude:'node_modules'
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: 'node_modules'
       }
     ]
   },
@@ -81,12 +81,12 @@ module.exports = {
 
     // new webpack.optimize.CommonsChunkPlugin('jquery', 'jquery.js'),
     //  new CommonsChunkPlugin('jquery.js'),
-    
+
     new HtmlwebpackPlugin({
       title: 'Ha.html html-webpack-plugin',
       filename: 'ha.html',
-      template:"./index.html",
-      inject: 'body' 
+      template: "./index.html",
+      inject: 'body'
     }),
 
   ],
