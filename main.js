266bfd29e7da95d35dfd7f69e597b6d333bdefef
wanglinzhizhi <wanglinzhizhi@hotmail.com>
2016-07-15 15:44:03 +0800

    好巧,上次测试git的时候用的也是这个专题

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var $=require('jquery');
 
 // var ha=require("./hah.js");
 // ha();
-
+require("./js/fixJquery.js");
 require("./js/jquery.mCustomScrollbar.min.js");
 require('./js/jquery.SuperSlide.js');
 var wanglinzhizhiTest=require('./js/wl.js');
